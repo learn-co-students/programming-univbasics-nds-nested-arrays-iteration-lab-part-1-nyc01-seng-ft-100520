8ab9608586f4263e42328611eb52b419a6ccc3b4
maxwellbenton <maxwellbenton@gmail.com>
2020-03-11 16:42:44 -0400

    fix starter code method name

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,4 @@
-def join_ingredients(src)
+def find_even_values(src)
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
   # Output all even values in each nested array
